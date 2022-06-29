da3e68d35daceee55cbd9bb8087dd13cf1e76b12
ezkam <kf.zakiyudin@gmal.com>
2022-06-29 13:28:52 +0700

    node-js-fundamen

---
+++ backend-fundamentals/lat_modularization/Wolf.js
@@ -0,0 +1,11 @@
+class Wolf{
+    constructor(){
+        this.strength = Math.floor(Math.random() * 100)
+    }
+
+    growl(){
+        console.log('auuuuuhhhhggggggg');
+    }
+}
+
+module.exports = Wolf
